be0a00252a2b390658794b2c902dcf2f379ea003
dennisobare <dennisobare338@gmail.com>
2024-02-27 23:23:21 +0300

    Excited about my first git commit ever!

---
+++ user_input.py
@@ -0,0 +1,6 @@
+# a simple program that asks a user to enter their name, age and location
+# and prints out a personalized message
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Where do you live? ")
+print("Hello",name + ",","you are",age,"years old and you come from",location + ".")
